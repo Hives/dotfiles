a5f16634c80f172034ccf4344d9c44615781d0ed
Paul Martin <paul.y.martin@johnlewis.co.uk>
2021-04-10 12:27:43 +0100

    xmonad: fix send window to screen 0

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -471,7 +471,7 @@ myKeys2 conf =
       [ ("M-s",   addName "Focus on next screen"  $ nextScreen)
       , ("M-S-s", addName "Move w to next screen" $ shiftNextScreen >> nextScreen)
       , ("M-w",   addName "Focus on screen 0"     $ viewScreen horizontalScreenOrderer 0)
-      , ("M-S-w", addName "Move w to screen 0"    $ sendToScreen def 0 >> viewScreen horizontalScreenOrderer 0)
+      , ("M-S-w", addName "Move w to screen 0"    $ sendToScreen horizontalScreenOrderer 0 >> viewScreen horizontalScreenOrderer 0)
       , ("M-e",   addName "Focus on screen 1"     $ viewScreen horizontalScreenOrderer 1)
       , ("M-S-e", addName "Move w to screen 1"    $ sendToScreen horizontalScreenOrderer 1 >> viewScreen horizontalScreenOrderer 1)
       , ("M-r",   addName "Focus on screen 2"     $ viewScreen horizontalScreenOrderer 2)
