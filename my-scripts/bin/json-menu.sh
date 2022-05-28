5e79d9e70fed55cbda8ea62d9dcba54594afec50
Paul Martin <paul.y.martin@johnlewis.co.uk>
2022-05-28 21:23:16 +0100

    Scripts: update json menu script

--- my-scripts/bin/json-menu.sh
+++ my-scripts/bin/json-menu.sh
@@ -1,12 +1,10 @@
 #! /usr/bin/env bash
 
-# requires launcher script - misnamed for this context
+# requires launcher script (not named well for this context)
 
 set -e -o pipefail
 
 menu_file=$1
-
-path=()
 
 # this is not very scientific - would be better remove only first and last
 # quote and de-escape quoted quotes
@@ -14,27 +12,21 @@ function remove_quotes {
     echo $1 | tr -d '"'
 }
 
-function select_keys {
-    object=$(cat $menu_file)
+object=$(cat $menu_file)
 
-    for path_segment in "${path[@]}"; do
-        # warning - breaks if certain characters in the key, e.g. "-"
-        object=$(echo $object | jq ".${path_segment}")
-    done
+function make_selection {
+    key=$(echo $object | jq -c keys | jq -c '.[]' | launcher)
+    key_unquoted=$(remove_quotes $key)
+
+    object=$(echo $object | jq ".${key_unquoted}")
 
     type=$(echo $object | jq -c 'type')
 
-    if [[ $type != '"object"' ]]; then
-        return
+    if [[ $type == '"object"' ]]; then
+        make_selection
     fi
-
-    new_path_segment=$(echo $object | jq -c keys | jq -c '.[]' | launcher)
-
-    path+=($(remove_quotes $new_path_segment))
-
-    select_keys
 }
 
-select_keys
+make_selection
 
 echo $(remove_quotes $object)
